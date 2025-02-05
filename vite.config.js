1e72e45670a0c408dd3f34e6714d768b664f506f
putraamir <d097839@siswa.upsi.edu.my>
2025-02-06 05:06:34 +0800

    hosting files 4

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
   plugins: [
+    vue(),
     laravel({
-      input: ["resources/css/app.css", "resources/js/app.js"],
+      input: ["resources/js/app.js", "resources/css/app.css"],
       refresh: true,
     }),
-    vue(),
   ],
   build: {
-    manifest: true,
     outDir: "public/build",
   },
 });
