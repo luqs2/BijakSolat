5aa19ccf562378a78fd6e8aca78ad31881807576
putraamir <d097839@siswa.upsi.edu.my>
2025-02-06 04:48:26 +0800

    remove duplicate

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,6 @@ Route::middleware('auth')->group(function () {
             'status' => session('status'),
         ]);
     })->name('login.page');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/kemaskini', function () {
         return Inertia::render('UpdateClass');
